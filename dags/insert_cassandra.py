21f14276b696f6af946b836179cc2dd754f3adaf
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2024-03-20 12:46:47 +0800

    enhance code for ingestion and data store

--- dags/insert_cassandra.py
+++ dags/insert_cassandra.py
@@ -2,46 +2,48 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.cassandra.hooks.cassandra import CassandraHook
-import uuid
 from datetime import datetime
 from uuid import uuid4
+import requests
+import json
 
 dag = DAG("Airflow_and_Cassandra",
     description="A simple DAG that helps to insert dummy data into Cassandra Database",
-    schedule_interval=timedelta(minutes=1),
+    schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=["ETL_jobs"])
 
-def insert_dummy_data_into_cassandra_db():
+def get_rest_api_data(**context):
+    task_instance = context["ti"]
+
+    url = "https://api.data.gov.my/data-catalogue?id=hies_state" 
+    response_json = requests.get(url=url).json()
+
+    task_instance.xcom_push("json_data", response_json)
+
+def insert_dummy_data_into_cassandra_db(**context):
+    task_instance = context["ti"]
+
+    Live_data = task_instance.xcom_pull(task_ids='get_rest_api_data',
+                                            key='json_data'
+                                            )
+
     hook = CassandraHook(cassandra_conn_id='cassandra_default')
     session = hook.get_conn()
 
-    # Generate some dummy data
-    dummy_data = [
-        {
-            "uuiid": uuid4(),
-            "source": "Dummy Source",
-            "author": "Dummy Author",
-            "category": "Dummy Category",
-            "country": "Dummy Country",
-            "description": "Dummy Description",
-            "image": "Dummy Image URL",
-            "language": "Dummy Language",
-            "published_at": datetime.now().isoformat(),  # Convert datetime to string
-            "title": "Dummy Title",
-            "url": "Dummy URL"
-        }
-    ]
-
-
-    # Insert dummy data
-    for data in dummy_data:
+    for data in Live_data:
         query = """
-            INSERT INTO news.news_table (uuiid, source, author, category, country, description, image, language, published_at, title, url)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            INSERT INTO hies.data_state (uuiid, date, state, gini, poverty, income_mean, income_median, expenditure_mean)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
         """
-        session.execute(query, (data["uuiid"], data["source"], data["author"], data["category"], data["country"], data["description"], data["image"], data["language"], data["published_at"], data["title"], data["url"]))
+        session.execute(query, (uuid4(), data["date"], data["state"], str(data["gini"]), str(data["poverty"]), str(data["income_mean"]), str(data["income_median"]), str(data["expenditure_mean"])))
+
+get_data_from_api = PythonOperator( 
+    task_id="get_rest_api_data", 
+    python_callable=get_rest_api_data, 
+    dag=dag
+)
 
 insert_dummy_data = PythonOperator(
     task_id="insert_dummy_data",
@@ -49,4 +51,4 @@ insert_dummy_data = PythonOperator(
     dag=dag,
 )
 
-insert_dummy_data
+get_data_from_api >> insert_dummy_data
